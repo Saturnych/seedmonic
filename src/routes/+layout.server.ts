99393d544d4cad5c0b8f499e786c99a64d9751ed
Saturnych <169702+Saturnych@users.noreply.github.com>
2024-04-23 01:09:06 +0300

    updated vars

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,12 +4,6 @@ import { DEBUG } from '$lib/vars/client';
 
 export const load = (async (event: Event) => {
 	const pathname = event.url?.pathname;
-
-	const num = getRandomInt(1, 2048);
-	if (DEBUG) console.log('getRandomInt:', num);
-
-	const blocks = getWordBlocks({ num });
-
 	return {
 		pathname,
 	};
