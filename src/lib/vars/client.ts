f070a8ebc1e6be9b09abf6fe979cc80c4ac329d0
Saturnych <169702+Saturnych@users.noreply.github.com>
2024-04-23 01:12:20 +0300

    Update client.ts

--- src/lib/vars/client.ts
+++ src/lib/vars/client.ts
@@ -1,8 +1,6 @@
-import { PUBLIC_APP_NAME, PUBLIC_ENV } from '$env/static/public';
-
 const ENV = Object.assign({}, import.meta.env);
 
-export const NODE_ENV: string = PUBLIC_ENV ?? ENV.NODE_ENV ?? ENV.VITE_USER_NODE_ENV ?? ENV.MODE ?? '';
+export const NODE_ENV: string = ENV.NODE_ENV ?? ENV.VITE_USER_NODE_ENV ?? ENV.MODE ?? '';
 export const DEV_MODE: boolean = (!!NODE_ENV && NODE_ENV.indexOf('dev') > -1) ?? ENV.DEV;
 export const DEBUG = !!DEV_MODE;
 export const SSR = !!ENV.SSR;
@@ -12,4 +10,4 @@ export const LANG: string = ENV.LANG ?? 'en_US.UTF-8';
 export const PORT = Number(ENV.PORT ?? 3030);
 export const BASEURI: string = ENV.BASEURI ?? 'http://127.0.0.1:3030';
 export const SESSION_TTL = Number(ENV.SESSION_TTL ?? 24 * 3600 * 1000); // day in ms
-export const APP_NAME: string = PUBLIC_APP_NAME ?? ENV.APP_NAME ?? 'App';
+export const APP_NAME: string = ENV.APP_NAME ?? 'App';
