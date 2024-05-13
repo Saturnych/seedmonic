ed06b3b2eccd219b23c0fb12fa072b451f06fcaf
Saturnych <169702+Saturnych@users.noreply.github.com>
2024-05-13 20:38:25 +0300

    added favicons

---
+++ src/lib/components/Head.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import consts from '$lib/vars/consts';
+
+	export let title = 'bip39 wordlist';
+</script>
+
+<svelte:head>
+	<title>{title} // {consts.title}</title>
+	<style>
+		@import url('https://fonts.cdnfonts.com/css/sf-mono?styles=36554,36555,36556');
+	</style>
+</svelte:head>
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,20 +1,18 @@
 <script lang="ts">
 	import { dev } from '$app/environment';
+	import Head from '$lib/components/Head.svelte';
 	import Metrika from '$lib/components/Metrika.svelte';
+	import Posthog from '$lib/components/Posthog.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import consts from '$lib/vars/consts';
 	import './app.css';
 </script>
 
-<svelte:head>
-	<title>bip39 wordlist // {consts.title}</title>
-	<style>
-		@import url('https://fonts.cdnfonts.com/css/sf-mono?styles=36554,36555,36556');
-	</style>
-</svelte:head>
+<Head />
 
 {#if !dev}
 	<Metrika />
+	<Posthog />
 {/if}
 
 <div class="relative mx-auto flex min-h-screen max-w-6xl flex-col gap-2 px-8 pb-12 font-mono sm:pb-16">
