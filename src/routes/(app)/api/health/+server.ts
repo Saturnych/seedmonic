07585a7dee854624f027600e213ad21ba517129c
Saturnych <169702+Saturnych@users.noreply.github.com>
2024-12-15 15:29:26 +0300

    Update +server.ts

--- src/routes/(app)/api/health/+server.ts
+++ src/routes/(app)/api/health/+server.ts
@@ -2,12 +2,6 @@ import { getWordBlocks, returnJson } from '$lib/utils';
 import consts from '$lib/vars/consts';
 
 export const GET = async () => {
-	const data = {
-		status: 200,
-		success: true,
-		message: 'ok',
-		version: consts.version,
-	};
 	return returnJson({
 		timestamp: Date.now(),
 		uptime: process?.uptime(),
