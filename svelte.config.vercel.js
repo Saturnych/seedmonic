b398b458f655f989836d3697ae18143865964a38
Saturnych <169702+Saturnych@users.noreply.github.com>
2024-12-16 04:21:36 +0300

    adapters updated

--- svelte.config.vercel.js
+++ svelte.config.vercel.js
@@ -1,7 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,22 +7,15 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [vitePreprocess()],
 	kit: {
-		appDir: 'app', // Required as the default is _app
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH
-		},
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'index.html' // may differ from host to host
+			runtime: 'edge',
+			memory: 1024
 		}),
 		prerender: {
 			handleHttpError: ({ path, referrer, message }) => {
-				/*
-				// ignore deliberate link to shiny 404 page
-				if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
-					return;
-				}
-				*/
-
 				// otherwise fail the build
 				throw new Error(message);
 			}
