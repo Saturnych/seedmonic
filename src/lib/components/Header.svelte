ec3e4fd9283f479bd4b8d43d76467019f15e576f
Denis <saturnych@gmail.com>
2025-07-17 13:22:55 +0300

    Update Header.svelte

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,6 +4,6 @@
 
 <div class="mt-auto">
 	<div class="flex flex-wrap items-center justify-left text-neutral-50/20 gap-4 py-4">
-		<a href="/" title="home">home</a> | <a href="/bip39" title="Home">bip39 words</a>
+		<a href="/" title="home">home</a> | <a href="/bip39" title="bip39 words">bip39 words</a>
 	</div>
 </div>
